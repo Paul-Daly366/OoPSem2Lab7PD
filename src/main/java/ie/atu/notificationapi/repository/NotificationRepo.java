78b66c135930d79c59e7f2d2c21b342543c7e533
PaulDaly-STUDENT <g00470372@atu.ie>
2026-03-23 11:58:55 +0000

    Finished notification model, service and controller

--- src/main/java/ie/atu/notificationapi/repository/NotificationRepo.java
+++ src/main/java/ie/atu/notificationapi/repository/NotificationRepo.java
@@ -1,4 +1,7 @@
 package ie.atu.notificationapi.repository;
 
-public class NotificationRepo {
+import ie.atu.notificationapi.model.Notification;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface NotificationRepo extends JpaRepository<Notification,Long> {
 }
